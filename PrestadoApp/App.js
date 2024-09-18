53303e28bd5cfe2f68b8099e6a3c27eff27ed193
Matias Cancina <cancinamatias@gmail.com>
2024-09-18 18:08:44 -0300

    nativewind

--- PrestadoApp/App.js
+++ PrestadoApp/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color: 'white'}}>Welcome to Prestado App!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 items-center justify-center bg-black'>
+      <Text className='text-white text-2xl font-bold'>Bienvenido a Prestado App Mate!</Text>
+      <StatusBar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
