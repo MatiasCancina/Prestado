a5db9286db8373639b034e76f23f3c85a47caa2e
Matias Cancina <cancinamatias@gmail.com>
2024-09-19 14:49:14 -0300

    screens

---
+++ PrestadoApp/screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-2xl font-bold">
+        Bienvenido a Prestado App Mate!
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
