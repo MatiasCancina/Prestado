71b3a82ee792489ecadacd1c1ef79884dea124ae
Matias Cancina <cancinamatias@gmail.com>
2024-09-23 17:21:15 -0300

    feat(searchBar)

--- PrestadoApp/screens/ItemsListScreen.jsx
+++ PrestadoApp/screens/ItemsListScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ActivityIndicator, FlatList } from "react-native";
+import { View, Text, ActivityIndicator, FlatList, TextInput } from "react-native";
 import { db } from "../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import ItemCard from "../components/ItemCard";
@@ -7,6 +7,7 @@ import ItemCard from "../components/ItemCard";
 const ItemsListScreen = () => {
   const [itemsList, setItemsList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     const getItemsList = async () => {
@@ -50,12 +51,33 @@ const ItemsListScreen = () => {
     );
   }
 
+  // Filtrar items según el texto de búsqueda
+  const filteredItems = itemsList.filter((item) => {
+    const searchTextLower = searchText.toLowerCase();
+    return (
+      item.name.toLowerCase().includes(searchTextLower) ||
+      item.category.toLowerCase().includes(searchTextLower) ||
+      (item.location &&
+        `${item.location.latitude}, ${item.location.longitude}`.includes(
+          searchTextLower
+        ))
+    );
+  });
+
   return (
     <View className="flex-1 bg-gray-100 p-4">
       <Text className="text-2xl font-bold mb-4">Items List</Text>
-      {itemsList.length > 0 ? (
+
+      <TextInput
+        value={searchText}
+        onChangeText={setSearchText}
+        placeholder="Search by name, category or location"
+        className="border-b p-2 mb-4"
+      />
+
+      {filteredItems.length > 0 ? (
         <FlatList
-          data={itemsList}
+          data={filteredItems}
           renderItem={({ item }) => <ItemCard item={item} />}
           keyExtractor={(item) => item.id}
           className="w-full"
