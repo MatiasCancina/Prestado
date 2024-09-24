fd2916e75c0ee4490b89881ab179abfb37a1c7e6
Matias Cancina <cancinamatias@gmail.com>
2024-09-24 17:15:40 -0300

    feat(asyncStorage)

--- PrestadoApp/context/AuthContext.jsx
+++ PrestadoApp/context/AuthContext.jsx
@@ -1,68 +1,3 @@
-// import React, { createContext, useContext, useEffect, useState } from "react";
-// import { auth } from "../firebaseConfig";
-// import {
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-//   onAuthStateChanged,
-//   signOut,
-// } from "firebase/auth";
-// import { useNavigation } from "@react-navigation/native";
-
-// const AuthContext = createContext();
-
-// export const useAuthContext = () => useContext(AuthContext);
-
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-//   const navigation = useNavigation();
-
-//   const registerUser = async (email, password) => {
-//     try {
-//       await createUserWithEmailAndPassword(auth, email, password);
-//     } catch (error) {
-//       console.error("Error registering user:", error.message);
-//     }
-//   };
-
-//   const loginUser = async (email, password) => {
-//     try {
-//       await signInWithEmailAndPassword(auth, email, password);
-//     } catch (error) {
-//       console.error("Error logging in:", error.message);
-//     }
-//   };
-
-//   const logout = async () => {
-//     try {
-//       await signOut(auth);
-//       navigation.navigate("Login");
-//     } catch (error) {
-//       console.error("Error logging out:", error.message);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         setUser(user);
-//         navigation.navigate("Home");
-//       } else {
-//         setUser(null);
-//         navigation.navigate("Login");
-//       }
-//     });
-
-//     return () => unsubscribe();
-//   }, [navigation]);
-
-//   return (
-//     <AuthContext.Provider value={{ user, registerUser, loginUser, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export default AuthContext;
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "../firebaseConfig";
 import {
@@ -72,7 +7,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { useNavigation } from "@react-navigation/native";
-import AsyncStorage from '@react-native-async-storage/async-storage'; // Importar AsyncStorage
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AuthContext = createContext();
 
@@ -116,7 +51,7 @@ export const AuthProvider = ({ children }) => {
   const registerUser = async (email, password) => {
     try {
       const { user } = await createUserWithEmailAndPassword(auth, email, password);
-      storeUser(user); // Guardar el usuario en AsyncStorage
+      storeUser(user);
     } catch (error) {
       console.error("Error registering user:", error.message);
     }
@@ -125,7 +60,7 @@ export const AuthProvider = ({ children }) => {
   const loginUser = async (email, password) => {
     try {
       const { user } = await signInWithEmailAndPassword(auth, email, password);
-      storeUser(user); // Guardar el usuario en AsyncStorage
+      storeUser(user);
     } catch (error) {
       console.error("Error logging in:", error.message);
     }
@@ -134,7 +69,7 @@ export const AuthProvider = ({ children }) => {
   const logout = async () => {
     try {
       await signOut(auth);
-      removeUser(); // Eliminar el usuario de AsyncStorage
+      removeUser();
       navigation.navigate("Login");
     } catch (error) {
       console.error("Error logging out:", error.message);
@@ -148,11 +83,11 @@ export const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        storeUser(user); // Guardar el usuario en AsyncStorage
+        storeUser(user); 
         navigation.navigate("Home");
       } else {
         setUser(null);
-        removeUser(); // Eliminar el usuario de AsyncStorage si se desloguea
+        removeUser(); 
         navigation.navigate("Login");
       }
     });
