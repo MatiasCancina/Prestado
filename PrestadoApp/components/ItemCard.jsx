fd28e60b8f66a61547d07e31ea5c4628daf41789
Matias Cancina <cancinamatias@gmail.com>
2024-09-24 19:27:04 -0300

    feat(RequestLoan)

--- PrestadoApp/components/ItemCard.jsx
+++ PrestadoApp/components/ItemCard.jsx
@@ -1,18 +1,33 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
-const ItemCard = ({ item }) => (
-  <View className="bg-blue-300 rounded-lg p-4 mb-4 shadow-md">
-    <Text className="text-lg font-bold mb-2">{item.name}</Text>
-    <Text className="text-gray-600">Category: {item.category}</Text>
-    <Text className="text-gray-600">Description: {item.description}</Text>
-    <Text className="text-gray-600">
-      Availability: {item.availability ? "Available" : "Not Available"}
-    </Text>
-    <Text className="text-gray-600">Rating: {item.rating}</Text>
-    <Text className="text-gray-600">
-      Location: {item.location.latitude}, {item.location.longitude}
-    </Text>
-  </View>
-);
-
+const ItemCard = ({ item, navigation }) => {
+  return (
+    <View className="flex flex-row justify-between bg-blue-300 rounded-lg p-4 mb-4 shadow-md">
+      <View>
+        <Text className="text-lg font-bold mb-2">{item.name}</Text>
+        <Text className="text-gray-600">Category: {item.category}</Text>
+        <Text className="text-gray-600">Description: {item.description}</Text>
+        <Text className="text-gray-600">
+          Availability: {item.availability ? "Available" : "Not Available"}
+        </Text>
+        <Text className="text-gray-600">Rating: {item.rating}</Text>
+        <Text className="text-gray-600">
+          Location: {item.location.latitude}, {item.location.longitude}
+        </Text>
+      </View>
+      <View>
+        <Button
+          title="Solicitar Préstamo"
+          onPress={() =>
+            navigation.navigate("RequestLoan", {
+              itemId: item.id,
+              lenderId: item.lenderId,
+              itemName: item.name
+            })
+          }
+        />
+      </View>
+    </View>
+  );
+};
 export default ItemCard;
