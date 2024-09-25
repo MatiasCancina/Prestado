187ab784a38d48c4998daac8b552ab5f0c63c3bc
Matias Cancina <cancinamatias@gmail.com>
2024-09-25 11:13:15 -0300

    feat(LoanManagement navigation)

--- PrestadoApp/navigation/ItemsStackNavigator.jsx
+++ PrestadoApp/navigation/ItemsStackNavigator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ItemsListScreen from "../screens/ItemsListScreen";
 import RequestLoanScreen from "../screens/RequestLoanScreen";
+import LoanManagementScreen from "../screens/LoanManagementScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const ItemsStackNavigator = () => {
     >
       <Stack.Screen name="ItemsList" component={ItemsListScreen} />
       <Stack.Screen name="RequestLoan" component={RequestLoanScreen} />
+      <Stack.Screen name="LoanManagement" component={LoanManagementScreen} />
     </Stack.Navigator>
   );
 };
