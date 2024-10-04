985fe8c7653ef570173ef5b55fa7d7fe2f9c60bd
Matias Cancina <cancinamatias@gmail.com>
2024-10-03 21:37:20 -0300

    fix(coords format)

--- PrestadoApp/components/AddItemForm.jsx
+++ PrestadoApp/components/AddItemForm.jsx
@@ -1,3 +1,338 @@
+// import React, { useEffect, useState } from "react";
+// import {
+//   View,
+//   Text,
+//   TextInput,
+//   Alert,
+//   Image,
+//   ScrollView,
+//   StyleSheet,
+//   TouchableOpacity,
+// } from "react-native";
+// import { db, storage } from "../firebaseConfig";
+// import { addDoc, collection } from "firebase/firestore";
+// import { useAuthContext } from "../context/AuthContext";
+// import * as ImagePicker from "expo-image-picker";
+// import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+// import { Feather } from "@expo/vector-icons";
+
+// const AddItemForm = ({ navigation }) => {
+//   const [name, setName] = useState("");
+//   const [category, setCategory] = useState("");
+//   const [description, setDescription] = useState("");
+//   const [location, setLocation] = useState({ latitude: "", longitude: "" });
+//   const [availability, setAvailability] = useState(true);
+//   const [rating, setRating] = useState(0);
+//   const [imageUri, setImageUri] = useState(null);
+//   const { user } = useAuthContext();
+
+//   const requestPermissions = async () => {
+//     const { status: cameraStatus } =
+//       await ImagePicker.requestCameraPermissionsAsync();
+//     const { status: galleryStatus } =
+//       await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+//     if (cameraStatus !== "granted" || galleryStatus !== "granted") {
+//       Alert.alert(
+//         "Missing Permissions",
+//         "Permissions are required to access the camera and gallery.",
+//         [{ text: "Ok" }]
+//       );
+//     }
+//   };
+
+//   useEffect(() => {
+//     requestPermissions();
+//   }, []);
+
+//   const pickImageFromGallery = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ImagePicker.MediaTypeOptions.Images,
+//       allowsEditing: true,
+//       aspect: [4, 3],
+//       quality: 1,
+//     });
+
+//     if (!result.canceled) {
+//       setImageUri(result.assets[0].uri);
+//     }
+//   };
+
+//   const takePhoto = async () => {
+//     let result = await ImagePicker.launchCameraAsync({
+//       allowsEditing: true,
+//       aspect: [4, 3],
+//       quality: 1,
+//     });
+
+//     if (!result.canceled) {
+//       setImageUri(result.assets[0].uri);
+//     }
+//   };
+
+//   const generateUniqueId = () => {
+//     return `${Date.now()}-${Math.floor(Math.random() * 1000)}`;
+//   };
+
+//   const handleSubmit = async () => {
+//     if (rating === 0) {
+//       Alert.alert("Error", "Please select a rating for the item.");
+//       return;
+//     }
+
+//     try {
+//       let imageUrl = "";
+//       if (imageUri) {
+//         const imageName = `items/${generateUniqueId()}`;
+//         const storageRef = ref(storage, imageName);
+//         const img = await fetch(imageUri);
+//         const bytes = await img.blob();
+//         await uploadBytes(storageRef, bytes);
+//         imageUrl = await getDownloadURL(storageRef);
+//       }
+
+//       const itemsRef = collection(db, "items");
+
+//       await addDoc(itemsRef, {
+//         name,
+//         category,
+//         description,
+//         location: {
+//           latitude: parseFloat(location.latitude),
+//           longitude: parseFloat(location.longitude),
+//         },
+//         availability,
+//         rating,
+//         lenderId: user.uid,
+//         imageUrl,
+//         createdAt: new Date(),
+//       });
+
+//       navigation.navigate("ItemsList");
+//       Alert.alert("Success", "Item added successfully!");
+//     } catch (error) {
+//       Alert.alert("Error", "An error occurred while adding the item.");
+//     }
+//   };
+
+//   const RatingStars = () => {
+//     return (
+//       <View style={styles.ratingContainer}>
+//         {[1, 2, 3, 4, 5].map((star) => (
+//           <TouchableOpacity
+//             key={star}
+//             onPress={() => setRating(star)}
+//           >
+//             <Feather
+//               name='star'
+//               size={30}
+//               color={star <= rating ? "#FFD700" : "#6C63FF"}
+//               style={styles.starIcon}
+//             />
+//           </TouchableOpacity>
+//         ))}
+//       </View>
+//     );
+//   };
+
+//   return (
+//     <ScrollView
+//       style={styles.container}
+//       contentContainerStyle={{ flexGrow: 1 }}
+//     >
+//       <Text style={styles.title}>Add New Item</Text>
+
+//       <View style={styles.inputContainer}>
+//         <Feather
+//           name="box"
+//           size={20}
+//           color="#6C63FF"
+//           style={styles.inputIcon}
+//         />
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Name"
+//           value={name}
+//           onChangeText={setName}
+//         />
+//       </View>
+
+//       <View style={styles.inputContainer}>
+//         <Feather
+//           name="tag"
+//           size={20}
+//           color="#6C63FF"
+//           style={styles.inputIcon}
+//         />
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Category"
+//           value={category}
+//           onChangeText={setCategory}
+//         />
+//       </View>
+
+//       <View style={styles.inputContainer}>
+//         <Feather
+//           name="file-text"
+//           size={20}
+//           color="#6C63FF"
+//           style={styles.inputIcon}
+//         />
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Description"
+//           value={description}
+//           onChangeText={setDescription}
+//           multiline
+//         />
+//       </View>
+
+//       <View style={styles.inputContainer}>
+//         <Feather
+//           name="map-pin"
+//           size={20}
+//           color="#6C63FF"
+//           style={styles.inputIcon}
+//         />
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Latitude"
+//           value={location.latitude}
+//           onChangeText={(text) => setLocation({ ...location, latitude: text })}
+//           keyboardType="numeric"
+//         />
+//       </View>
+
+//       <View style={styles.inputContainer}>
+//         <Feather
+//           name="map-pin"
+//           size={20}
+//           color="#6C63FF"
+//           style={styles.inputIcon}
+//         />
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Longitude"
+//           value={location.longitude}
+//           onChangeText={(text) => setLocation({ ...location, longitude: text })}
+//           keyboardType="numeric"
+//         />
+//       </View>
+
+//       <Text style={styles.ratingLabel}>Rating:</Text>
+//       <RatingStars />
+
+//       <View style={styles.imageButtonsContainer}>
+//         <TouchableOpacity
+//           style={styles.imageButton}
+//           onPress={pickImageFromGallery}
+//         >
+//           <Feather name="image" size={20} color="#FFFFFF" />
+//           <Text style={styles.imageButtonText}>Choose Image</Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity style={styles.imageButton} onPress={takePhoto}>
+//           <Feather name="camera" size={20} color="#FFFFFF" />
+//           <Text style={styles.imageButtonText}>Take Photo</Text>
+//         </TouchableOpacity>
+//       </View>
+
+//       {imageUri && (
+//         <Image source={{ uri: imageUri }} style={styles.previewImage} />
+//       )}
+
+//       <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+//         <Text style={styles.submitButtonText}>Add Item</Text>
+//       </TouchableOpacity>
+//     </ScrollView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     padding: 20,
+//     backgroundColor: "#F0F0F7",
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     color: "#333",
+//     marginBottom: 20,
+//     paddingTop: 30,
+//     textAlign: "center",
+//   },
+//   inputContainer: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     backgroundColor: "#FFFFFF",
+//     borderRadius: 10,
+//     marginBottom: 15,
+//     paddingHorizontal: 10,
+//   },
+//   inputIcon: {
+//     marginRight: 10,
+//   },
+//   input: {
+//     flex: 1,
+//     paddingVertical: 12,
+//     fontSize: 16,
+//   },
+//   ratingLabel: {
+//     fontSize: 16,
+//     fontWeight: "bold",
+//     color: "#333",
+//     marginBottom: 10,
+//   },
+//   ratingContainer: {
+//     flexDirection: "row",
+//     justifyContent: "center",
+//     marginBottom: 20,
+//   },
+//   starIcon: {
+//     marginHorizontal: 5,
+//   },
+//   imageButtonsContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//     marginBottom: 15,
+//   },
+//   imageButton: {
+//     backgroundColor: "#6C63FF",
+//     flexDirection: "row",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     padding: 12,
+//     borderRadius: 10,
+//     flex: 1,
+//     marginHorizontal: 5,
+//   },
+//   imageButtonText: {
+//     color: "#FFFFFF",
+//     marginLeft: 5,
+//     fontSize: 16,
+//   },
+//   previewImage: {
+//     width: "100%",
+//     height: 200,
+//     borderRadius: 10,
+//     marginBottom: 15,
+//   },
+//   submitButton: {
+//     backgroundColor: "#6C63FF",
+//     padding: 15,
+//     borderRadius: 10,
+//     alignItems: "center",
+//   },
+//   submitButtonText: {
+//     color: "#FFFFFF",
+//     fontSize: 18,
+//     fontWeight: "bold",
+//   },
+// });
+
+// export default AddItemForm;
+
 import React, { useEffect, useState } from "react";
 import {
   View,
@@ -76,7 +411,16 @@ const AddItemForm = ({ navigation }) => {
 
   const handleSubmit = async () => {
     if (rating === 0) {
-      Alert.alert("Error", "Please select a rating for the item.");
+      Alert.alert("Error", "Por favor, selecciona una calificación para el ítem.");
+      return;
+    }
+
+    // Validar las coordenadas
+    const lat = parseFloat(location.latitude);
+    const lon = parseFloat(location.longitude);
+
+    if (isNaN(lat) || isNaN(lon) || lat < -90 || lat > 90 || lon < -180 || lon > 180) {
+      Alert.alert("Error", "Por favor, ingresa coordenadas válidas. La latitud debe estar entre -90 y 90, y la longitud entre -180 y 180.");
       return;
     }
 
@@ -98,8 +442,8 @@ const AddItemForm = ({ navigation }) => {
         category,
         description,
         location: {
-          latitude: parseFloat(location.latitude),
-          longitude: parseFloat(location.longitude),
+          latitude: lat,
+          longitude: lon,
         },
         availability,
         rating,
@@ -109,9 +453,9 @@ const AddItemForm = ({ navigation }) => {
       });
 
       navigation.navigate("ItemsList");
-      Alert.alert("Success", "Item added successfully!");
+      Alert.alert("Éxito", "Ítem agregado correctamente!");
     } catch (error) {
-      Alert.alert("Error", "An error occurred while adding the item.");
+      Alert.alert("Error", "Ocurrió un error al agregar el ítem.");
     }
   };
 
@@ -197,7 +541,7 @@ const AddItemForm = ({ navigation }) => {
         />
         <TextInput
           style={styles.input}
-          placeholder="Latitude"
+          placeholder="Latitud (ej: 40.7128)"
           value={location.latitude}
           onChangeText={(text) => setLocation({ ...location, latitude: text })}
           keyboardType="numeric"
@@ -213,7 +557,7 @@ const AddItemForm = ({ navigation }) => {
         />
         <TextInput
           style={styles.input}
-          placeholder="Longitude"
+          placeholder="Longitud (ej: -74.0060)"
           value={location.longitude}
           onChangeText={(text) => setLocation({ ...location, longitude: text })}
           keyboardType="numeric"
--- PrestadoApp/screens/MapScreen.jsx
+++ PrestadoApp/screens/MapScreen.jsx
@@ -1,5 +1,88 @@
-import React from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
+// import React from "react";
+// import { View, StyleSheet, Image, Text } from "react-native";
+// import MapView, { Marker, Callout } from "react-native-maps";
+
+// const CustomMarker = ({ item }) => (
+//   <View style={styles.markerContainer}>
+//     <Image source={{ uri: item.imageUrl }} style={styles.markerImage} />
+//   </View>
+// );
+
+// const MapScreen = ({ route }) => {
+//   const { items } = route.params;
+
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         style={styles.map}
+//         initialRegion={{
+//           latitude: items[0]?.location?.latitude || 0,
+//           longitude: items[0]?.location?.longitude || 0,
+//           latitudeDelta: 0.05,
+//           longitudeDelta: 0.05,
+//         }}
+//       >
+//         {items.map((item) => (
+//           <Marker
+//             key={item.id}
+//             coordinate={{
+//               latitude: item.location.latitude,
+//               longitude: item.location.longitude,
+//             }}
+//           >
+//             <CustomMarker item={item} />
+//             <Callout>
+//               <View style={styles.calloutContainer}>
+//                 <Text style={styles.calloutTitle}>{item.name}</Text>
+//                 <Text style={styles.calloutDescription}>{item.description}</Text>
+//               </View>
+//             </Callout>
+//           </Marker>
+//         ))}
+//       </MapView>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   map: {
+//     flex: 1,
+//   },
+//   markerContainer: {
+//     width: 50,
+//     height: 50,
+//     borderRadius: 25,
+//     overflow: 'hidden',
+//     backgroundColor: 'white',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderWidth: 2,
+//     borderColor: '#6C63FF',
+//   },
+//   markerImage: {
+//     width: 46,
+//     height: 46,
+//     borderRadius: 23,
+//   },
+//   calloutContainer: {
+//     width: 200,
+//     padding: 10,
+//   },
+//   calloutTitle: {
+//     fontWeight: 'bold',
+//     fontSize: 16,
+//     marginBottom: 5,
+//   },
+//   calloutDescription: {
+//     fontSize: 14,
+//   },
+// });
+
+// export default MapScreen;import React from "react";
+import { View, StyleSheet, Image, Text, Alert } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 
 const CustomMarker = ({ item }) => (
@@ -11,18 +94,34 @@ const CustomMarker = ({ item }) => (
 const MapScreen = ({ route }) => {
   const { items } = route.params;
 
+  // Filtrar items con coordenadas válidas
+  const validItems = items.filter(item =>
+    item.location &&
+    typeof item.location.latitude === 'number' &&
+    typeof item.location.longitude === 'number' &&
+    !isNaN(item.location.latitude) &&
+    !isNaN(item.location.longitude)
+  );
+
+  if (validItems.length === 0) {
+    Alert.alert("No hay ítems para mostrar", "No se encontraron ítems con ubicaciones válidas.");
+    return null;
+  }
+
+  const initialRegion = {
+    latitude: validItems[0].location.latitude,
+    longitude: validItems[0].location.longitude,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  };
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: items[0]?.location?.latitude || 0,
-          longitude: items[0]?.location?.longitude || 0,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
+        initialRegion={initialRegion}
       >
-        {items.map((item) => (
+        {validItems.map((item) => (
           <Marker
             key={item.id}
             coordinate={{
