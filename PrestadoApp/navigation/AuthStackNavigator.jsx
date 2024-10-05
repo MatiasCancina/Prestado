d265e9b613f479f7e6f937bf09ca027bdf432889
Matias Cancina <cancinamatias@gmail.com>
2024-10-05 13:08:35 -0300

    fix(auth navigation)

--- PrestadoApp/navigation/AuthStackNavigator.jsx
+++ PrestadoApp/navigation/AuthStackNavigator.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { ItemProvider } from '../context/ItemContext'
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import AddItemScreen from '../screens/AddItemScreen';
-import ItemsListScreen from '../screens/ItemsListScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
@@ -15,9 +12,6 @@ const AuthStackNavigator = () => {
           <Stack.Navigator initialRouteName="Login">
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="AddItem" component={AddItemScreen} />
-            <Stack.Screen name="ItemsList" component={ItemsListScreen} />
           </Stack.Navigator>
         </ItemProvider>
   )
